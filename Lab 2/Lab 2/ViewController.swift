7dfcdc3df60c2cfdcaa3c5c8537957c17fd3cbaf
shahjay182 <50597556+shahjay182@users.noreply.github.com>
2019-09-20 10:56:08 -0400

    last

---
+++ Lab 2/Lab 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab 2
+//
+//  Created by Tom Marazzo on 2019-09-12.
+//  Copyright © 2019 GeorgianCollege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
